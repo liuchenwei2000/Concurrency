153af178d901febf9abbdd79ae4d8988da4b1e23
Vic Liu <liuchenwei2000@163.com>
2015-01-19 21:01:26 +0800

    PriorityBlockingQueue、DelayQueue part complete

---
+++ src/collections/collections/DelayQueueTest.java
@@ -0,0 +1,142 @@
+/**
+ * 
+ */
+package collections;
+
+import java.util.Date;
+import java.util.concurrent.DelayQueue;
+import java.util.concurrent.Delayed;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * DelayQueue 示例
+ * <p>
+ * DelayQueue 类是 Delayed 元素的一个无界阻塞队列，只有在延迟期满时才能从中提取该元素。
+ * 遍历队列的相关方法会忽略那些延迟的对象，直到这些对象因为期满被激活。
+ * 
+ * @author 刘晨伟
+ * 
+ * 创建日期：2015年1月16日
+ */
+public class DelayQueueTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		DelayQueue<Event> queue = new DelayQueue<>();
+
+		// 先使用 5 个线程添加元素
+		Thread[] threads = new Thread[5];
+		for (int i = 0; i < threads.length; i++) {
+			threads[i] = new Thread(new Task(i, queue));
+			threads[i].start();
+		}
+
+		// 等待线程全部结束
+		for (Thread thread : threads) {
+			try {
+				thread.join();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+
+		System.out.printf("Main: Queue Size: %d\n", queue.size());
+		
+		// 每隔 500ms 打印一次队列内的元素
+		do {
+			int counter = 0;
+			Event event;
+			do {
+				// 获取并移除此队列的头，如果此队列不包含具有已到期延迟时间的元素，则返回 null。
+				event = queue.poll();
+				// 获取并移除此队列的头部，在可从此队列获得到期延迟的元素之前一直等待（如有必要）。
+//				event = queue.take();
+				// 获取但不移除此队列的头部；如果此队列为空，则返回 null。
+//				event = queue.peek();
+				if (event != null) {
+					counter++;
+				}
+			} while (event != null);
+			System.out.printf("At %s you have read %d events\n", new Date(), counter);
+			try {
+				TimeUnit.MILLISECONDS.sleep(500);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		} while (queue.size() > 0);// 注意 size() 方法将返回所有元素数（包括延时对象和非延时对象）
+
+		// 完全移除此队列中的所有元素。
+//				queue.clear();
+		System.out.printf("Main: End of the program\n");
+	}
+	
+	/**
+	 * 存入 DelayQueue 的元素需要实现 Delayed 接口，该接口用来标记那些应该在给定延迟时间之后执行的对象。
+	 */
+	private static class Event implements Delayed {
+
+		private Date activationDate;// 对象激活日期
+
+		public Event(Date activationDate) {
+			this.activationDate = activationDate;
+		}
+
+		/**
+		 * 返回与此对象相关的剩余延迟时间，以给定的时间单位表示。
+		 * 
+		 * @see java.util.concurrent.Delayed#getDelay(java.util.concurrent.TimeUnit)
+		 */
+		@Override
+		public long getDelay(TimeUnit unit) {
+			Date now = new Date();
+			long diff = activationDate.getTime() - now.getTime();
+			return unit.convert(diff, TimeUnit.MILLISECONDS);
+		}
+		
+		/**
+		 * Delayed 接口继承自 Comparable 接口，所以要实现下面的比较方法
+		 * 
+		 * @see java.lang.Comparable#compareTo(java.lang.Object)
+		 */
+		@Override
+		public int compareTo(Delayed o) {
+			long result = this.getDelay(TimeUnit.NANOSECONDS)
+					- o.getDelay(TimeUnit.NANOSECONDS);
+			if (result < 0) {
+				return -1;
+			} else if (result > 0) {
+				return 1;
+			}
+			return 0;
+		}
+	}
+
+	/**
+	 * 添加元素的任务
+	 */
+	private static class Task implements Runnable {
+
+		private int id;
+		private DelayQueue<Event> queue;
+		
+		public Task(int id, DelayQueue<Event> list) {
+			this.id = id;
+			this.queue = list;
+		}
+
+		@Override
+		public void run() {
+			Date now = new Date();
+			Date delay = new Date(now.getTime() + (id * 1000));
+			System.out.printf("Thread %s: %s\n", id, delay);
+			// 添加 100 个延时对象
+			for (int i = 0; i < 100; i++) {
+				queue.add(new Event(delay));
+				// 将指定元素插入此延迟队列。 
+//				events.put(new Event(id, i));
+			}
+		}
+	}
+}
---
+++ src/collections/collections/PriorityBlockingQueueTest.java
@@ -0,0 +1,134 @@
+/**
+ * 
+ */
+package collections;
+
+import java.util.concurrent.PriorityBlockingQueue;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * PriorityBlockingQueue 示例
+ * <p>
+ * PriorityBlockingQueue 是并发环境下使用的有序（无界）列表，其内元素需要实现 Comparable 接口。
+ * 当插入元素时，PriorityBlockingQueue 会根据元素实现的 compareTo() 方法来决定它的插入位置，越大的元素越靠后。
+ * 此外，PriorityBlockingQueue 是阻塞队列，如果某些操作（比如队列为空时使用poll()方法
+ * 获取并删除一个元素）不能立即执行则将会阻塞线程直到操作完成。
+ * 
+ * @author 刘晨伟
+ * 
+ * 创建日期：2015年1月16日
+ */
+public class PriorityBlockingQueueTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		PriorityBlockingQueue<Event> queue = new PriorityBlockingQueue<>();
+		
+		// 先使用 5 个线程添加元素
+		Thread[] threads = new Thread[5];
+		for (int i = 0; i < threads.length; i++) {
+			threads[i] = new Thread(new Task(i, queue));
+			threads[i].start();
+		}
+		
+		// 等待线程全部结束
+		for (Thread thread : threads) {
+			try {
+				thread.join();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		System.out.printf("Main: Queue Size: %d\n",queue.size());
+		// 主线程开始从 PriorityBlockingQueue 取出元素
+		for (int i = 0; i < threads.length * 100; i++) {
+			// 获取并移除此队列的头，如果此队列为空，则返回 null。
+			Event event = queue.poll();
+			System.out.println(event);
+		}
+		
+		System.out.println("501 = " + queue.poll());
+		System.out.printf("Main: Queue Size: %d\n", queue.size());
+		
+		try {
+			// 获取并移除此队列的头，在指定的等待时间前等待可用的元素（如果有必要）。
+			Event event = queue.poll(5, TimeUnit.SECONDS);
+			System.out.println(event == null ? "time out" : event);
+			// 获取并移除此队列的头部，在元素变得可用之前一直等待（如果有必要）。
+//			event = queue.take();
+			// 获取但不移除此队列的头；如果此队列为空，则返回 null。
+//			event = queue.peek();
+			// 完全移除此队列中的所有元素。
+//			queue.clear();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		System.out.printf("Main: End of the program\n");
+	}
+	
+	private static class Event implements Comparable<Event> {
+
+		private int thread;
+		private int priority;
+		
+		public Event(int thread, int priority) {
+			this.thread = thread;
+			this.priority = priority;
+		}
+
+		/**
+		 * 实现比较方法
+		 * <p>
+		 * 返回值说明：<p>
+		 * 1，表示this对象小于参数对象。
+		 * 0，表示this对象等于参数对象。
+		 * -1，表示this对象大于参数对象。
+		 * 
+		 * @see java.lang.Comparable#compareTo(java.lang.Object)
+		 */
+		@Override
+		public int compareTo(Event o) {
+			if (this.priority > o.getPriority()) {
+				return -1;
+			} else if (this.priority < o.getPriority()) {
+				return 1;
+			}
+			return 0;
+		}
+
+		public int getPriority() {
+			return priority;
+		}
+
+		@Override
+		public String toString() {
+			return "Event [thread=" + thread + ", priority=" + priority + "]";
+		}
+	}
+
+	/**
+	 * 添加元素的任务
+	 */
+	private static class Task implements Runnable {
+
+		private int id;
+		private PriorityBlockingQueue<Event> events;
+		
+		public Task(int id, PriorityBlockingQueue<Event> list) {
+			this.id = id;
+			this.events = list;
+		}
+
+		@Override
+		public void run() {
+			for (int i = 0; i < 100; i++) {
+				events.add(new Event(id, i));
+				// 将指定元素插入此优先级队列。该队列是无界的，所以此方法不会阻塞。 
+//				events.put(new Event(id, i));
+			}
+		}
+	}
+}
